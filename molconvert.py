8c01ce97be66310bc49a755b30fc247efef92df9
Olav Vahtras <vahtras@kth.se>
2014-03-22 15:21:37 +0100

    pylint cleanup

--- molconvert.py
+++ molconvert.py
@@ -10,26 +10,30 @@ import argparse
 import os
 import zmat
 
-class UnsupportedMoltype(Exception): pass
+class UnsupportedMoltype(Exception): 
+    """Basic exception for unregognized mol types"""
+    pass
 # make the class return error output?
 
 FROMS = ['.zmat', ]
 TOS = ['.xyz', '.mol', '.pot']
+ALPHA_ALKENE = {"C":0.878, "H": 0.135, "O": 0.465}
 
 
 def main(*args, **kwargs):
-    """Convert molecule file foo to bar"""
-    foo, bar = args
+    """Convert molecule file from_mol to to_mol"""
+    from_mol, to_mol = args
     #
     # Determine type of from file by extension
     #
-    _, from_ext  = os.path.splitext(foo)
-    if from_ext not in FROMS: raise(UnsupportedMoltype)
+    _, from_ext  = os.path.splitext(from_mol)
+    if from_ext not in FROMS: 
+        raise(UnsupportedMoltype)
 
     #
     # Determine output file by extension
     #
-    _, to_ext = os.path.splitext(bar)
+    _, to_ext = os.path.splitext(to_mol)
     if to_ext not in TOS: 
         print to_ext, TOS
         raise(UnsupportedMoltype)
@@ -37,7 +41,7 @@ def main(*args, **kwargs):
 
     #Process input file
     if from_ext == '.zmat':
-        lines = [line.strip() for line in open(foo).readlines()]
+        lines = [line.strip() for line in open(from_mol).readlines()]
         mol = zmat.Mol(lines)
         mol.update_cartesian()
         #
@@ -45,12 +49,18 @@ def main(*args, **kwargs):
         alist = [atom for atom in mol.atomlist if atom.label != "X"]
 
         #Open output flie
-        with open(bar, 'w') as outf:
+        with open(to_mol, 'w') as outf:
             if to_ext  == '.xyz':
-                outf.write("%d\nGenerated from %s by molconvert\n" % (len(alist), foo, ))
+                outf.write(
+                    "%d\nGenerated from %s by molconvert\n" % (
+                        len(alist), from_mol
+                        )
+                    )
                 for atom in alist:
                     x, y, z = atom.coor[:]
-                    outf.write("%s %10.6f %10.6f %10.6f\n"%(atom.label, x, y, z))
+                    outf.write(
+                        "%s %10.6f %10.6f %10.6f\n" % (atom.label, x, y, z)
+                        )
                     print("%s %10.6f %10.6f %10.6f\n"%(atom.label, x, y, z))
             elif to_ext == '.pot':
                 outf.write("AA\n%d 1 0 1\n" % (len(alist),))
@@ -66,7 +76,7 @@ def main(*args, **kwargs):
 Generated from %s by molconvert
 ====================================
 Atomtypes=%d Units=Angtrom Nosymmetry
-""" % (foo, len(mol.atomtypes))
+""" % (from_mol, len(mol.atomtypes))
                     )
                 for atype in mol.atomtypes.keys():
                     atoms = mol.atomtypes[atype]
@@ -78,9 +88,15 @@ Atomtypes=%d Units=Angtrom Nosymmetry
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('foo')
-    parser.add_argument('bar')
-    args = parser.parse_args()
+    parser.add_argument('from_mol')
+    parser.add_argument('to_mol')
+    parser.add_argument('--alpha_C', default=ALPHA_ALKENE["C"])
+    parser.add_argument('--alpha_H', default=ALPHA_ALKENE["H"])
+    parser.add_argument('--alpha_O', default=ALPHA_ALKENE["O"])
+    pargs = parser.parse_args()
     # defalt
-    kwargs = {"C":0.878, "H": 0.135, "O": 0.465}
-    main(args.foo, args.bar, **kwargs) 
+    main(
+        pargs.from_mol, pargs.to_mol, 
+        C=pargs.alpha_C, H=pargs.alpha_H, O=pargs.alpha_O
+        )
+       
