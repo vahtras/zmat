8d79ad9b53060a28fc66d639e761b9d23c883096
Olav Vahtras <vahtras@kth.se>
2016-10-12 19:09:37 +0200

    Better setup

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     author="Olav Vahtras",
     author_email="olav.vahtras@gmail.com",
     version="1.0",
+    py_modules=["zmat", "molconvert"],
+    scripts=["molconvert"],
     install_requires=["blocked-matrix-utils"],
     )
